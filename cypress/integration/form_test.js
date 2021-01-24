ca0f7140fe89b34dbdadf983f827826f25f79fe6
Saeed Khan <saeedkhan@Saeeds-MacBook-Pro.local>
2021-01-24 16:37:51 -0700

    Cypress testing complete

---
+++ cypress/integration/form_test.js
@@ -0,0 +1,25 @@
+describe('Name Input Field Test', function () {
+    it('Checks to see if their is an input for name', function () {
+        cy.get('input[name="firstName"]').should('have.placeholder', 'Name')
+    })
+})
+
+describe('Email Input Checker', function () {
+    it('checks to see if their is an email input', function () {
+        cy.get("[type='email']")
+        cy.get("[type='email']").type('myemail@domain.com')
+    }) 
+})
+
+describe('Password Input Checker', function () {
+    it('checks to see if their is an password input', function () {
+        cy.get("[type='password']")
+        cy.get("[type='password']").type('Hello123!')
+    }) 
+})
+
+describe("Is Box Checked?", function () {
+    it("Checks to see if user will give away all their data", function () {
+        cy.get('[type="checkbox"]').check(true && false) 
+    })
+})
